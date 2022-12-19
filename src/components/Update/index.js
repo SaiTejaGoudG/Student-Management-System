060855c56f43ba4273927ad3b431e15c4da2f052
Sai Teja Goud <108140708+SaiTejaGoudG@users.noreply.github.com>
2022-12-20 03:13:17 +0530

    Update index.js

--- src/components/Update/index.js
+++ src/components/Update/index.js
@@ -1,12 +1,12 @@
 import React , {useState, useEffect} from 'react'
-import { useLocation } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 import firebasedb from '../../firebase';
-import { useNavigate } from 'react-router-dom';
 
 const UpdateDetails = () => {
-  // getting all the queries
+  // getting Query Parameters
     let query = new URLSearchParams(useLocation().search);
     let navigate = useNavigate()
+
     const [key,setKey]= useState(null)
     const [data,setData] = useState({
         firstName: '',
@@ -15,13 +15,12 @@ const UpdateDetails = () => {
         emailId: '',
         address: '',
     })
-    // destructuring
-    const {firstName,lastName,phoneNo,emailId,address,addDetailsError} = {...data}
-  
+    
     const changeHandler = event => {
         setData({...data, [event.target.id]: event.target.value})
     }
-    // default values from Home
+
+    // getting values from Home page 
     useEffect(()=>{setKey(query.get('key'))},[])
 
     // updating values
@@ -29,24 +28,18 @@ const UpdateDetails = () => {
       if(key){
         firebasedb.child(`register/${key}`).on('value',data=>{
           const dbData= data.val()
-          console.log(dbData)
           setData(dbData)
-         })
-      }
-        
+        })
+      }   
     },[key])
 
     // Navigating to Home page with updated values
     const submitHandler = event => {
-        firebasedb.child(`register/${key}`).update(data,err=>{
-          if(err){
-            console.log(err)
-          }else{
-            navigate('/Home')
-
-          }
-        })
+      firebasedb.child(`register/${key}`).update(data,navigate('/Home'))
     }
+
+    const {firstName,lastName,phoneNo,emailId,address} = {...data}
+
     return (
         <div className="bg-container">
       <div className='topnav' style={{width:'100vw'}}>
@@ -112,13 +105,9 @@ const UpdateDetails = () => {
             onChange={changeHandler}
             value={address}
             id="address" />
-            {addDetailsError === true ? (
-            <p className="errMsg">*Enter Valid Details</p>
-          ) : null}
         </div>
         <div className="button-container">
           <input className="button" value="save" type="button" onClick={submitHandler}/>
-           
         </div>
         <br />
       </form>
